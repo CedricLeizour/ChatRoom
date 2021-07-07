f58d21d64fc286a275373daf4d37b9b6377b43a7
CedricLeizour <cedricleizour@gmail.com>
2021-07-07 15:46:12 +0200

    props done

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import './styles.scss';
 
-const Message = ({}) => (
-  <div className="message">
+const Message = ({ author, message, isOther }) => (
+  <div className={classNames('message', { 'message--other': isOther })}>
     <div className="message__author">
-      Super chat
+      { author }
     </div>
     <div className="message__body">
-      Salut ça va ?
+      { message }
     </div>
   </div>
 );
+
+Message.propTypes = {
+  author: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  isOther: PropTypes.bool,
+};
+
+Message.defaultProps = {
+  isOther: false,
+};
 
 export default Message;
--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Message from './Message';
 import './styles.scss';
 
-const Messages = ({}) => (
+const Messages = ({ messages }) => (
   <div className="messages">
-    <Message />
-    <Message />
-    <Message />
+    {messages.map((message) => (
+      <Message {...message} key={message.id} />
+    ))}
   </div>
 );
+
+Messages.propTypes = {
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ),
+};
+
+Messages.defaultProps = {
+  messages: [],
+};
 
 export default Messages;
