379987eaf92fa5f2b8f7cbe6c868749b1f9e6d08
CedricLeizour <cedricleizour@gmail.com>
2021-07-07 16:14:46 +0200

    initialState done

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Send } from 'react-feather';
+import PropTypes from 'prop-types';
 import './styles.scss';
 
-const Form = ({}) => (
+const Form = ({ inputValue }) => (
   <form className="form">
   <input 
   type="text" 
-  className="form__input" 
+  className="form__input"
+  value= {inputValue} 
   placeholder="Saisissez votre message"
   />
   <button type submit="submit" className="form__submit">
@@ -14,5 +16,9 @@ const Form = ({}) => (
   </button>
   </form>
 );
+
+Form.propTypes = {
+   inputValue: PropTypes.string.isRequired,
+};
 
 export default Form;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,25 @@
-const reducer = (state = [], action) => {
+const INITIAL_STATE = {
+  currentMessage: '',
+  messages: [{
+    id: 1,
+    author: 'Super Chat',
+    message: 'Salut, ça va ?',
+    isOther: false,
+  },
+  {
+    id: 2,
+    author: 'Super Chat',
+    message: 'Salut, t\'as pas des super croquettes ?',
+    isOther: false,
+  },
+  {
+    id: 3,
+    author: 'Super Chat',
+    message: 'Stp !',
+    isOther: false,
+  }],
+};
+const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     default:
       return state;
